555fd351b74e1a3c37ceb3d524109bed37dad6e4
fingersnow <dingyongbiao123@163.com>
2018-02-05 09:45:24 +0800

    update storage file 1.0

--- fastdfs/run.sh
+++ fastdfs/run.sh
@@ -1,6 +1,6 @@
-!/bin/bash
+#!/bin/bash
 
 sed -i 's/tracker_server=.*/tracker_server='`hostname`':22122/g' /etc/fdfs/storage.conf
-/opt/apache-tomcat-7.0.78/bin/startup.sh
 /usr/bin/fdfs_trackerd /etc/fdfs/tracker.conf
 /usr/bin/fdfs_storaged /etc/fdfs/storage.conf
+
